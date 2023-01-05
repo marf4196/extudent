41df07c0ccb4965feb7ca1cf4f2e2b22517565ce
Sobhan Zadehali <99137872+soza780@users.noreply.github.com>
2023-01-05 12:55:50 -0800

    Update views.py

--- extudent/message/api/v1/views.py
+++ extudent/message/api/v1/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import get_object_or_404
 
 
 class MessageGenericApiView(generics.ListCreateAPIView):
+    
+    """ messages that has been recieved from admin"""
+    
     queryset = Message.objects.filter(is_recived=True)
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -14,4 +17,18 @@ class MessageGenericApiView(generics.ListCreateAPIView):
         queryset = self.get_queryset()
         obj = get_object_or_404(queryset, user=self.request.user)
         return obj
-        
+
+
+class MessageGenericApiView(generics.ListCreateAPIView):
+    """ messages that has been sent to support """
+    queryset = Message.objects.filter(is_recived=False)
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        queryset = self.get_queryset()
+        obj = get_object_or_404(queryset, user=self.request.user)
+        return obj
+    
+# view for writing and sending messages too all users by admin
+###############################################################
